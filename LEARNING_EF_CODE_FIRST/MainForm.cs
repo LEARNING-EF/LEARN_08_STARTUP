5fa048d259b0431f2167785ed3c5af9ee71877cd
Dariush Tasdighi <dariush.tasdighi@hotmail.com>
2018-09-13 10:21:22 +0430

    Add project files.

---
+++ LEARNING_EF_CODE_FIRST/MainForm.cs
@@ -0,0 +1,47 @@
+﻿namespace LEARNING_EF_CODE_FIRST
+{
+	public partial class MainForm : System.Windows.Forms.Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+
+		private void MainForm_Load(object sender, System.EventArgs e)
+		{
+			Models.DatabaseContext databaseContext = null;
+
+			try
+			{
+				databaseContext =
+					new Models.DatabaseContext();
+
+				Models.Country country = new Models.Country();
+
+				// Solution (1)
+				//country.Id = System.Guid.NewGuid();
+				// /Solution (1)
+
+				country.Name = "Iran";
+
+				databaseContext.Countries.Add(country);
+
+				databaseContext.SaveChanges();
+
+				System.Guid newId = country.Id;
+			}
+			catch (System.Exception ex)
+			{
+				System.Windows.Forms.MessageBox.Show(ex.Message);
+			}
+			finally
+			{
+				if (databaseContext != null)
+				{
+					databaseContext.Dispose();
+					databaseContext = null;
+				}
+			}
+		}
+	}
+}
---
+++ Models/Country.cs
@@ -0,0 +1,46 @@
+﻿namespace Models
+{
+	public class Country : object
+	{
+		public Country() : base()
+		{
+			// Solution (2)
+			//Id = System.Guid.NewGuid();
+			// /Solution (2)
+		}
+
+		// Solution (1)
+		//[System.ComponentModel.DataAnnotations.Schema.DatabaseGenerated
+		//	(System.ComponentModel.DataAnnotations.Schema.DatabaseGeneratedOption.None)]
+		//public System.Guid Id { get; set; }
+		// /Solution (1)
+
+		// Solution (2)
+		//[System.ComponentModel.DataAnnotations.Schema.DatabaseGenerated
+		//	(System.ComponentModel.DataAnnotations.Schema.DatabaseGeneratedOption.None)]
+		//public System.Guid Id { get; set; }
+		// /Solution (2)
+
+		// Solution (3)
+		[System.ComponentModel.DataAnnotations.Schema.DatabaseGenerated
+			(System.ComponentModel.DataAnnotations.Schema.DatabaseGeneratedOption.Identity)]
+		public System.Guid Id { get; set; }
+		// Solution (3)
+
+		//// Solution (4)
+		//// Note: Does Not Supported in EF 6.2.0 But Supported in EF Core 2.0
+		////[System.ComponentModel.DataAnnotations.Schema.DatabaseGenerated
+		////	(System.ComponentModel.DataAnnotations.Schema.DatabaseGeneratedOption.Computed)]
+		////public System.Guid Id { get; set; }
+		//// Solution (4)
+
+		// Solution (4)
+		// Note: Does Not Supported in EF 6.2.0 But Supported in EF Core 2.0
+		//[System.ComponentModel.DataAnnotations.Schema.DatabaseGenerated
+		//	(System.ComponentModel.DataAnnotations.Schema.DatabaseGeneratedOption.Computed)]
+		//public System.DateTime InsertDateTime { get; set; }
+		// Solution (4)
+
+		public string Name { get; set; }
+	}
+}
